7678d2bab6d27252bc579704044448f51f0ca52b
GuuiBenny92 <guilherme.caetano1992@gmail.com>
2024-05-27 15:56:35 -0300

    meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,16 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // To do: Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pela classe scanner os valores digitados no terminal
+
+        // Exibir a mensagem da conta criada
+
+        int numero = 1;
+        double numero2 = 22.1;
+
+        System.out.println("impimindo numero: " + numero);
     }
 }
